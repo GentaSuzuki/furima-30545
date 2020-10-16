94731efa567caef189d29331082f295292f9f41b
GentaSuzuki <71617008+GentaSuzuki@users.noreply.github.com>
2020-10-16 12:28:08 +0900

    コードの保守性向上

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :item_find, only: [:edit, :update]
+  before_action :item_find, only: [:edit, :update, :destroy]
   def index
     @items = Item.all.order('created_at DESC')
   end
@@ -36,10 +36,11 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    @item = Item.find(params[:id])
-    @item.destroy
-    if user_signed_in? && @item.user_id
-    redirect_to root_path
+    if current_user.id == @item.user_id
+      @item.destroy
+      redirect_to root_path
+    else
+      render :show
     end
   end
 
