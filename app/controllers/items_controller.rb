6ac4888dfdcd25932fd73e7f5b0e5e5b59481e92
GentaSuzuki <71617008+GentaSuzuki@users.noreply.github.com>
2020-10-16 11:13:00 +0900

    コードの可読性向上＆直接editを防止

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,8 +23,8 @@ class ItemsController < ApplicationController
   end
 
   def edit
-     if user_signed_in? && current_user.id == @item.user_id
-      render :edit
+     if current_user.id != @item.user_id
+      return redirect_to root_path
      else
       render :new
      end
